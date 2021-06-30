e64a223c05fac776a580496de3ceb86f74f1102e
Haotian Huang <haotianhuang@gmail.com>
2021-06-30 11:18:12 +1000

    Initial Commit

---
+++ Final Challenge/Final_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ChallengeApp.swift
+//  Final Challenge
+//
+//  Created by Haotian Huang on 30/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Final_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
